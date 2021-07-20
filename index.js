d07b4159b21d0cf6f90033cdbbfbe1749da0742b
Nastusya-max <Nastusya-max>
2021-07-20 23:15:38 +0300

    my new changes

---
+++ index.js
@@ -0,0 +1,7 @@
+var yodasay = require('yodasay');
+var lolcatjs = require('lolcatjs');
+var shakespeare = require('shakespeare-data');
+
+lolcatjs.fromString(yodasay.say({
+  text: shakespeare.sonnets.random().lines.join('\n')
+}));
