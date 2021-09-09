105cf39ac17864ecf690cef60ebdc3082f075fbf
Nastya Chernyavskaya <55358461+Nastusya-max@users.noreply.github.com>
2021-09-09 13:22:43 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,81 @@
-let a = [1, 2, 3, 4];
-console.log(approx(a));
+let arr = [1, 2, 0, 4];
+console.log('One-dimensional array:\n', arr);
+console.log('Approximated one-dimensional array:\n', approx(arr));
+
+
+let arr2D = [[0, 2, 3], [1, 5, 6], [7, 8, 0]];
+console.log('Two-dimensional array:\n', arr2D);
+console.log('Approximated two-dimensional array:\n',approx2D(arr2D));
+
+let arr3D = [
+  [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ],
+  [
+    [1, 2, 3],
+    [4, 1, 6],
+    [7, 5, 1],
+  ],
+  [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ],
+];
+console.log('Three-dimensional array:\n', arr3D);
+console.log('Approximated three-dimensional array:\n', approx3D(arr3D));
+
+// if an element with such an index is not found
+function check(item) {
+  if (!(item == undefined)) {
+    return item;
+  } else {
+    return 0;
+  }
+}
 
 function approx(arr) {
   arrApprox = [];
 
   arr.forEach(function (item, i, arr) {
-    switch (i) {
-      case (0):
-        item = arr[i + 1];
-        arrApprox.push(item);
-        break;
-      case (arr.length - 1):
-        item = arr[i - 1];
-        arrApprox.push(item);
-        break;
-      default:
-        item = arr[i - 1] + arr[i + 1];
-        arrApprox.push(item);
-        break;
-    }
+    item = check(arr[i - 1]) + check(arr[i + 1]);
+    arrApprox.push(item);
   });
   return arrApprox;
 }
 
+function approx2D(arr) {
+  var approx2D = new Array();
 
+  for (var i = 0; i < arr.length; i++) {
+    approx2D[i] = new Array();
+
+    for (var j = 0; j < arr[i].length; j++) {
+      approx2D[i][j] = check(check(arr[i + 1])[j]) + check(check(arr[i - 1])[j]) + 
+      check(check(arr[i])[j - 1]) + check(check(arr[i])[j + 1]);
+    }
+  }
+  return approx2D;
+}
+
+function approx3D(arr) {
+  var approx3D = new Array();
+
+  for(var i = 0; i < arr.length; i++){
+    approx3D[i] = new Array();
+
+    for(var j = 0; j < arr[i].length; j++){
+      approx3D[i][j] = new Array();
+
+      for(var k = 0; k < arr[i][j].length; k++){
+        // approx2D[i][j][k] =  data[i][j][k]; 
+        approx3D[i][j][k] =  check((check(check(arr[i + 1])[j]))[k]) + check((check(check(arr[i - 1])[j]))[k]) + 
+        check((check(check(arr[i])[j + 1]))[k]) + check((check(check(arr[i])[j - 1]))[k]) +
+        check((check(check(arr[i])[j]))[k + 1]) + check((check(check(arr[i])[j]))[k - 1]); 
+      }
+    }
+  }
+  return approx3D;
+}
