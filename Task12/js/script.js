5574ac9f6943c86f2ae3bfa98ce1695f45feff92
Nastusya-max <Nastusya-max>
2021-10-12 00:16:52 +0300

    second dropdown

--- Task12/js/script.js
+++ Task12/js/script.js
@@ -1,31 +1,18 @@
-const select = document.querySelector('.select-category')
+const selectCategory = document.querySelector('.select-category')
+const selecTitle = document.querySelector('.select-title')
 
-const getCategory = async () => {
-  let categories
+const getData = async (str) => {
+  let data
   try {
-    const res = await fetch('https://api.publicapis.org/categories')
+    const res = await fetch(str)
     if (!res.ok) {
       throw new error(res.statusText)
     }
-    categories = await res.json()
+    data = await res.json()
   } catch ({ message }) {
     throw new Error(message)
   }
-  return categories
-}
-
-const getEntries = async (str) => {
-  let entries
-  try {
-    const res = await fetch(`https://api.publicapis.org/entries?category=${str}&https=true`)
-    if (!res.ok) {
-      throw new error(res.statusText)
-    }
-    entries = await res.json()
-  } catch ({ message }) {
-    throw new Error(message)
-  }
-  return entries
+  return data
 }
 
 const outputCategory = (arr) => {
@@ -33,33 +20,68 @@ const outputCategory = (arr) => {
     let opt = document.createElement('option')
     opt.classList.add('category')
     opt.textContent = arr[i]
-    select.appendChild(opt)
+    selectCategory.appendChild(opt)
+  }
+}
+
+const outputTitle = (arr) => {
+  // deleting the titles of the previous category
+  for (let i = selecTitle.children.length - 1; i >= 0; --i) {
+    if (selecTitle.children[i].classList == 'title') {
+      selecTitle.children[i].remove()
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    let opt = document.createElement('option')
+    opt.classList.add('title')
+    opt.textContent = arr[i]
+    selecTitle.appendChild(opt)
   }
 }
 
 const outputEntries = () => {
-  select.onchange = function () {
+  // for category
+  selectCategory.onchange = function () {
     if (this.options[this.selectedIndex].classList == 'category') {
-      getEntries(this.options[this.selectedIndex].textContent)
-        .then(entries => {
-          document.querySelector(".output").innerHTML = `Count of entries: ${entries.count} <br>`
-          entries.entries.map((item, i) => document.querySelector(".output").innerHTML += `${i + 1}) API: ${item.API}; description: ${item.Description}; <br>`)
+      let categoryName = this.options[this.selectedIndex].textContent
+
+      getData(`https://api.publicapis.org/entries?category=${categoryName}&https=true`)
+        .then(res => {
+          let arrAPI = []
+          res.entries.map(item => arrAPI.push(item.API))
+          outputTitle(arrAPI)
         })
-        .catch(({ message }) => console.log("Error:", message))
+        .catch(({ message }) => console.log('Error:', message))
     } else {
-      document.querySelector(".output").innerHTML = ''
+      document.querySelector('.output').innerHTML = ''
+    }
+    // for title
+    selecTitle.onchange = function () {
+      if (this.options[this.selectedIndex].classList == 'title') {
+        let titleName = this.options[this.selectedIndex].textContent
+
+        getData(`https://api.publicapis.org/entries?title=${titleName}&https=true`)
+          .then(res => {
+            document.querySelector('.output').innerHTML = `Count of entries: ${res.count} <br>`
+            res.entries.map((item) => document.querySelector('.output').innerHTML += `description: ${item.Description}; <br>`)
+          })
+          .catch(({ message }) => console.log('Error:', message))
+      } else {
+        document.querySelector('.output').innerHTML = ''
+      }
     }
   }
 }
 
 window.onload = () => {
-  getCategory()
+  getData('https://api.publicapis.org/categories')
     .then(category => {
       promise = new Promise((resolve) => {
         resolve(outputCategory(category))
       })
         .then(outputEntries())
-        .catch(err => { console.log("Error:", err) })
+        .catch(err => { console.log('Error:', err) })
     })
-    .catch(({ message }) => console.log("Error:", message))
+    .catch(({ message }) => console.log('Error:', message))
 }
